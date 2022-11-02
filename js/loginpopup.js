51329f55f44712cdea5a5b5c6bca5f9df405f2ce
Otar7147 <114666455+Otar7147@users.noreply.github.com>
2022-11-02 20:44:50 +0300

    Add files via upload

--- js/loginpopup.js
+++ js/loginpopup.js
@@ -1,9 +1,16 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopup = document.querySelector('.login-popup');
+let popupClose = document.querySelector('.button-close');
 
-loginButton.addEventListener('click', showPopup);
+loginButton.addEventListener('click', showPopup) || popupClose.addEventListener('click', closePopup);
 
 function showPopup(evt) {
+    console.log(evt);
+    evt.preventDefault();
+    loginPopup.classList.toggle('show-popup');
+}
+
+function closePopup(evt) {
     console.log(evt);
     evt.preventDefault();
     loginPopup.classList.toggle('show-popup');
